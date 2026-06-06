59c2012710fa9a95033aa08b613f0dbc8e7d237e
jerometecson-main <jerometecson.main@gmail.com>
2026-06-06 23:46:59 +0800

    add proxy

--- app/backend/servers/icarus/route.ts
+++ app/backend/servers/icarus/route.ts
@@ -270,6 +270,7 @@ export async function GET(req: NextRequest) {
       );
 
     const proxies = [
+      "https://proxy.zxcprime380.workers.dev/",
       "https://mute-shadow-e0c7.zxcprime370.workers.dev/",
       "https://orange-tooth-0e36.zxcprime369.workers.dev/",
       "https://sweet-violet-4416.jerometecson99.workers.dev/",
